881be211b16694b23ff90aa8d2693d890dd5d8bf
ettayebSalim <selim.ettayeb@esprit.tn>
2022-05-31 11:20:20 +0100

    panier crud v0

---
+++ src/Default/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Default;
+import Services.PanierCRUD;
+import java.sql.DriverManager;
+import Utiles.MyConnection;
+
+/**
+ *
+ * @author 21622
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     //MyConnection mc = new MyConnection();
+     PanierCRUD paniercrud = new PanierCRUD();
+     paniercrud.ajouterPanier();
+    }
+    
+}
