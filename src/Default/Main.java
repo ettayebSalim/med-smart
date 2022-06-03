8a5d967ad3e2f6e22faf564eae4600263ed9646d
ettayebSalim <selim.ettayeb@esprit.tn>
2022-06-03 20:22:50 +0100

    fixer conflit

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -4,11 +4,16 @@
  * and open the template in the editor.
  */
 package Default;
+<<<<<<< HEAD
 import Models.User;
+=======
+import Models.Panier;
+>>>>>>> b0bca6e (panier crud)
 import Services.PanierCRUD;
 import Services.UserService;
 import java.sql.DriverManager;
 import Utiles.MyConnection;
+import java.sql.Date;
 
 /**
  *
@@ -19,11 +24,13 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
 //     MyConnection mc = new MyConnection();
     // PanierCRUD paniercrud = new PanierCRUD();
      //paniercrud.ajouterPanier();
-     
+             Date now = new Date(System.currentTimeMillis());
+
      UserService userService = new UserService();
      User user = new User("ettayeb","selim","selim.ettayeb@esprit.tn","14225520","azerty","22365478","Medecin");
   //  userService.ajouterUtilisateur(user);
@@ -31,6 +38,7 @@ public class Main {
  //    System.out.println(userService.getUserByID(3));
     // System.out.println(userService.deleteUser(2));
       userService.updateUser(user,3);
+
     }
     
 }
