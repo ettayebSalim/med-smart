33e1ad1af8069c9009e4bcefab90d34c5b7534f9
AymenGui <aymen.guizani@esprit.tn>
2022-06-05 00:13:17 +0100

    fichier

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -18,6 +18,7 @@ import java.io.PrintStream;
 
 import Services.ProduitService;
 import Models.Produit;
+import Services.FichierService;
 import java.sql.SQLException;
 
 /**
@@ -31,8 +32,8 @@ public class Main {
      */
 
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+MyConnection.getInstance();
 //     MyConnection mc = new MyConnection();
     // PanierCRUD paniercrud = new PanierCRUD();
      //paniercrud.ajouterPanier();
@@ -42,24 +43,28 @@ public class Main {
      //p.setId_prod(1);
      //ps.Supprimerproduits(p);
     //ps.insertProduit(p);
-//     UserService userService = new UserService();
-//     User user = new User("ettayeb","selim","selim.ettayeb@esprit.tn","14225520","azerty","22365478","Medecin");
-  //  userService.ajouterUtilisateur(user);
-//     System.out.println(userService.fetchUsers());
- //    System.out.println(userService.getUserByID(3));
-    // System.out.println(userService.deleteUser(2));
-//      userService.updateUser(user,3);
+    
+     //UserService userService = new UserService();
+    //User user = new User("ettayeb","selim","selim.ettayeb@esprit.tn","14225520","azerty","22365478","Medecin");
+   //userService.ajouterUtilisateur(user);
+   // System.out.println(userService.fetchUsers());
+   // System.out.println(userService.getUserByID(3));
+    //System.out.println(userService.deleteUser(2));
+      //userService.updateUser(user,3);
 
-   MyConnection.getInstance();
+   
         // PanierCRUD paniercrud = new PanierCRUD();
         //paniercrud.ajouterPanier();
 
 
-
-
+FichierService fs=new FichierService();
+   UserService userService = new UserService();
+   
+    User user = new User(1,"ettayeb","selim","selim.ettayeb@esprit.tn","14225520","azerty","22365478","Medecin");
+    userService.ajouterUtilisateur(user);
 //Fichier fichier=new Fichier("IRM","C:\\Users\\AGuizani\\Desktop\\test_care\\dataset\\00000141_000.png",user);
-     //  System.out.println(fichier);
-//FichierService fs=new FichierService();
+     // System.out.println(fichier);
+
 //fs.insertFichier(fichier);
 
     }
